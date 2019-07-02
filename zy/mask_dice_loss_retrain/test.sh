2554b0f8c3ec8e51b3551c84af88874067cb1152
zhongying <zhongying_m@zju.edu.cn>
2019-07-02 10:14:26 +0800

    test.sh

---
+++ zy/mask_dice_loss_retrain/test.sh
@@ -0,0 +1,4 @@
+cd /home/zhongying/reference/segment/maskrcnn-benchmark
+export NGPUS=2
+CUDA_VISIBLE_DEVICES='3' python tools/test_net.py \
+--config-file "/home/zhongying/reference/segment/maskrcnn-benchmark/zy/mask_dice_loss_retrain/e2e_mask_rcnn_R_50_FPN_1x_caffe2_mask_dice_loss_retrain.yaml"
